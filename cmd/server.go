60e37b989564b9baf6e820d97d7d17f1ac4d46b0
Matiss Kalnins <matiss.kalnins@enlabs.com>
2024-05-14 14:38:26 +0200

    Assetlinks file added

--- cmd/server.go
+++ cmd/server.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
 func main() {
 	app := echo.New()
+
+	app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	app.Use(middleware.Static("/.well-known/assetlinks.json"))
+
+	app.File("/.well-known/assetlinks.json", ".well-known/assetlinks.json")
 
 	app.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
